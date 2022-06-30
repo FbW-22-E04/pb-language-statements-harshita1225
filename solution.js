a3bd1824cbc5e77054c5da0cc7391c5af7150b2c
harshita <harshita1225@gmail.com>
2022-06-30 12:46:46 +0200

    task completed

---
+++ solution.js
@@ -0,0 +1,6 @@
+console.log('Hello World');
+console.log('This is my first js file');
+
+
+// if we remove parenthesis from the first statement,it results in an error
+//error says: syntax error:unexpected string.
